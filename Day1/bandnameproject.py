90894d0e759d8c7e7062c7228aa2ede1bff26174
ryzen729 <rahuulshahh@gmail.com>
2022-05-25 10:03:57 +0545

    project added

---
+++ Day1/bandnameproject.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+first_name = input("What's name of the city you grew up in?\n")
+last_name = input("What's your pet name?\n")
+print("Your band name could be " + first_name +" " + last_name)
