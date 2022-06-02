78413d205e44d1e8c097dae47a79072685c46b28
ryzen729 <rahuulshahh@gmail.com>
2022-06-02 17:38:01 +0545

    ceaserfirs

---
+++ Day8/ceaser.py
@@ -0,0 +1,31 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text,shift):
+  text_list = list()
+  for i in text:
+    text_list.append(i)
+  print(text_list)
+  
+  
+  encode_list = list()
+  for i in text_list:
+    index = 0
+    new_index = 0
+    for j in alphabet:
+      if i == j:
+        new_index = index + shift
+        if new_index >= 25:
+          remain_index = new_index - 25
+          new_index = remain_index -1
+        encode_list.append(alphabet[new_index])
+      index += 1
+  print(encode_list)
+
+encrypt(text,shift)       
+        
