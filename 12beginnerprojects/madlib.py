e8c28bf0136e10ff4ff505669c16a4e909f05f1a
ryzen729 <rahuulshahh@gmail.com>
2022-06-16 08:23:44 +0545

    snakeproject

---
+++ 12beginnerprojects/madlib.py
@@ -0,0 +1,12 @@
+
+
+adjective = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+
+madlib = f"Computer programming is so {adjective}! It makes me so exicted \
+all the time time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
